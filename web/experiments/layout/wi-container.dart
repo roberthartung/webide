b370c6b62c5107f6a75a649c17c5c8d1c49cd05c
Robert Hartung <r.hartung@roberthartung.de>
2014-05-16 16:10:39 +0200

    Observer and observable child list for wi-container

--- web/experiments/layout/wi-container.dart
+++ web/experiments/layout/wi-container.dart
@@ -7,13 +7,25 @@ import 'dart:html';
 @CustomTag('wi-container')
 class WiContainer extends PolymerElement {
 
-  WiContainer.created() : super.created();
+  MutationObserver _observer;
+  
+  WiContainer.created() : super.created() {
+    _observer = new MutationObserver(_onMutation);
+    _observer.observe(this, childList: true);
+  }
+  
+  _onMutation(List<MutationRecord> changes, MutationObserver) {
+    notifyPropertyChange(#observableChildren, null, children);
+    Observable.dirtyCheck();
+  }
   
   @published bool horizontal = false;
   
   @published bool vertical = false;
   
   @published bool resizable = false;
+  
+  @observable List get observableChildren => children; 
   
   void enteredView() {
     super.enteredView();
