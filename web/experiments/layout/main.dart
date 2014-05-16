2c17bb96e174c80efcc1ce64421c89bdff48055b
Robert Hartung <r.hartung@roberthartung.de>
2014-05-16 15:58:19 +0200

    Splitter insertion fixed - this is soo cool! ;)

--- web/experiments/layout/main.dart
+++ web/experiments/layout/main.dart
@@ -1,6 +1,18 @@
 import 'package:polymer/polymer.dart';
 
+import 'dart:html';
+
 @initMethod
 void main() {
+  ButtonElement add = querySelector('#add');
+  ButtonElement remove = querySelector('#remove');
+  HtmlElement container = querySelector('#container');
   
+  add.onClick.listen((MouseEvent e) {
+    container.append(new Element.tag('wi-dock'));
+  });
+  
+  remove.onClick.listen((MouseEvent e) {
+    container.children.removeLast();
+  });
 }
--- web/experiments/layout/wi-container.dart
+++ web/experiments/layout/wi-container.dart
@@ -6,6 +6,7 @@ import 'dart:html';
 
 @CustomTag('wi-container')
 class WiContainer extends PolymerElement {
+
   WiContainer.created() : super.created();
   
   @published bool horizontal = false;
@@ -24,23 +25,6 @@ class WiContainer extends PolymerElement {
     
     if(vertical)
         classes.add('vertical');
-    
-    if(resizable) {
-      Polymer.onReady.then((_) {
-        List<Element> _children = [];
-        Iterator it = children.skip(1).iterator;
-        while(it.moveNext()) {
-          _children.add(it.current);
-        }
-        
-        _children.forEach((Element e) {
-          WiSplitter splitter = new Element.tag('wi-splitter');
-          splitter.attributes['horizontal'] = horizontal ? 'true' : 'false';
-          splitter.attributes['vertical'] = vertical ? 'true' : 'false';
-          e.parent.insertBefore(splitter, e);
-        });
-      });
-    }
   }
 
   @override
