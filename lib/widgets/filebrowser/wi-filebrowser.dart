f080e55c9ed711efeb1ccea9508127f559684daa
Robert Hartung <robert-hartung@web.de>
2014-06-18 14:46:21 +0200

    Filebrowser started

---
+++ lib/widgets/filebrowser/wi-filebrowser.dart
@@ -0,0 +1,20 @@
+library webide.widgets.accordion;
+
+import 'package:polymer/polymer.dart';
+import 'package:chrome/chrome_app.dart';
+
+@CustomTag('wi-filebrowser')
+class WiFilebrowser extends PolymerElement {
+  WiFilebrowser.created() : super.created() {
+    
+  }
+  
+  void enteredView() {
+    super.enteredView();
+  }
+
+  @override
+  void ready() {
+    super.ready();
+  }
+}
